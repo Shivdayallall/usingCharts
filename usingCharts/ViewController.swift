4699e9809643aa2a17617f589a1d80bc361d91d0
shiv lall <shivlall0009@gmail.com>
2021-06-11 02:19:35 -0400

    configure bar and pie chart

--- usingCharts/ViewController.swift
+++ usingCharts/ViewController.swift
@@ -6,14 +6,75 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var pieChartView: PieChartView!
+    @IBOutlet weak var barChartView: BarChartView!
+    
+    
+    var players = ["ryan", "billy", "ted", "kevin", "randy"]
+    var scores = [5,2,56,87,98]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        customizeChart(dataPoints: players, values: scores.map{Double($0)})
+        customizeBarChart(dataPoints: players, values: scores.map{Double($0)})
 
+    }
+    
+    func customizeChart(dataPoints: [String], values: [Double]) {
+      
+      // 1. Set ChartDataEntry
+      var dataEntries: [ChartDataEntry] = []
+      for i in 0..<dataPoints.count {
+        let dataEntry = PieChartDataEntry(value: values[i], label: dataPoints[i], data: dataPoints[i] as AnyObject)
+        dataEntries.append(dataEntry)
+      }
+      // 2. Set ChartDataSet
+        let pieChartDataSet = PieChartDataSet(entries: dataEntries, label: nil)
+      pieChartDataSet.colors = colorsOfCharts(numbersOfColor: dataPoints.count)
+      // 3. Set ChartData
+      let pieChartData = PieChartData(dataSet: pieChartDataSet)
+      let format = NumberFormatter()
+      format.numberStyle = .none
+      let formatter = DefaultValueFormatter(formatter: format)
+      pieChartData.setValueFormatter(formatter)
+
+      // 4. Assign it to the chart’s data
+      pieChartView.data = pieChartData
+      pieChartView.backgroundColor = .brown
+    }
+    
+    // bar chart
+    func customizeBarChart(dataPoints: [String], values: [Double]) {
+      
+        var dataEntries: [BarChartDataEntry] = []
+        for i in 0..<dataPoints.count {
+          let dataEntry = BarChartDataEntry(x: Double(i), y: Double(values[i]))
+          dataEntries.append(dataEntry)
+        }
+        let chartDataSet = BarChartDataSet(entries: dataEntries, label: nil)
+        let chartData = BarChartData(dataSet: chartDataSet)
+        barChartView.data = chartData
+        barChartView.backgroundColor = .magenta
+    }
+    
+    
+    // color chart
+    private func colorsOfCharts(numbersOfColor: Int) -> [UIColor] {
+      var colors: [UIColor] = []
+      for _ in 0..<numbersOfColor {
+        let red = Double(arc4random_uniform(256))
+        let green = Double(arc4random_uniform(256))
+        let blue = Double(arc4random_uniform(256))
+        let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+        colors.append(color)
+      }
+      return colors
+    }
 
 }
 
